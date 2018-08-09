7d782dedfff29f62a39ee29a1717c81f5a33366b
hiroro-work <hiroro.work@gmail.com>
2018-08-09 11:11:01 +0000

    answer to no3

--- no3/ans.js
+++ no3/ans.js
@@ -1,28 +1,47 @@
 'use strict'
 
-const input = prompt('「じゃんけん・・・」\n> 0.グー 1.チョキ 2.パー')
-console.log(input)
-const computer = Math.floor(Math.random() * 3)
-console.log('「ぽい!」')
+const janken = ['グー', 'チョキ', 'パー']
 
-if (input === computer) {
-  // あいこ
-} else if (input === 0) {
-  if (computer === 1) {
-    // かち
-  } else if (computer === 2) {
-    // まけ
+function janken_poi() {
+  const input = Number(prompt('「じゃんけん・・・」\n> 0.グー 1.チョキ 2.パー'))
+  console.log(input)
+  const computer = Math.floor(Math.random() * 3)
+  console.log('「ぽい!」')
+  console.log(`*コンピュータ:${janken[computer]}`)
+  console.log(`*あなた:${janken[input]}`)
+
+  if (input === computer) {
+    console.log('「アイコでしょ！」')
+    janken_poi()
+    return
   }
-} else if (input === 1) {
-  if (computer === 2) {
-    // かち
-  } else if (computer === 0) {
-    // まけ
+
+  let win = false
+  switch (input) {
+    case 0:
+      if (computer === 1) {
+        win = true
+      }
+      break
+    case 1:
+      if (computer === 2) {
+        win = true
+      }
+      break
+    case 2:
+      if (computer === 0) {
+        win = true
+      }
+      break
+    default:
+      break
   }
-} else if (input === 2) {
-  if (computer === 0) {
-    // かち
-  } else if (computer === 1) {
-    // まけ
+
+  if (win) {
+    console.log('「あなたの勝ち！」')
+  } else {
+    console.log('「あなたの負け！」')
   }
 }
+
+janken_poi()
